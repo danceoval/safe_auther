dff290d510395c477364bf363342dcc856fe1844
Dan Sohval <dan@Dans-MacBook-Pro-2.local>
2016-10-26 10:44:53 -0400

    initial

---
+++ server/auth/index.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var router = express.Router();
+var User = require('../api/users/user.model');
+module.exports = router;
+
+router.post('/login', function (req, res, next) {
+  User.findOne({
+    where: req.body
+  })
+      .then(function (user) {
+        if (!user) {
+          res.sendStatus(401);
+        } else {
+          req.logIn(user, function (err) {
+              if (err) return next(err);
+              res.send(user);
+          });
+        }
+      });
+});
+
+router.get('/logout', function (req, res, next) {
+  req.logOut();
+  res.sendStatus(204);
+});
+
+router.post('/signup', function (req, res, next) {
+
+  User.findOrCreate({
+    where: {
+      email: req.body.email
+    },
+    defaults: {
+      password: req.body.password
+    }
+  })
+  .spread(function (user) {
+    req.logIn(user, function(err) {
+      if(err) return next(err);
+      res.send(user)
+    });
+  });
+
+});
+
+router.get('/me', function (req, res, next) {
+  if (req.user) {
+    res.send(req.user);
+  } else {
+    res.sendStatus(401);
+  }
+});
+
+router.use('/google', require('./google'));
